663a90cf77bfd62a760ea257e65b6cf9ff5a9878
nafies1 <ghuroba.muslim@gmail.com>
2020-01-10 16:11:00 +0700

    add get anime details

--- routes/anime.js
+++ routes/anime.js
@@ -3,8 +3,11 @@ const Anime = require('../controller/anime')
 
 
 router.get('/', Anime.showTopTen)
+router.get('/:idMal', Anime.getDetailsAnime)
 router.get('/search/:name', Anime.searchAnime)
 router.post('/', Anime.favAnime)
+// router.get('/', Anime.getFavAnime)
+
 
 module.exports = router
 
