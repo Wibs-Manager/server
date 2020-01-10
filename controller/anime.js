661043ca79f2c1906e9a64a0413b9a1962eb583c
mcc-17 <mcc-17@mcc-17s-Mac-mini.local>
2020-01-10 14:27:29 +0700

    add favorite mal

--- controller/anime.js
+++ controller/anime.js
@@ -4,6 +4,8 @@ const jikanApi = axios.create({
     // timeout: 10000,
     // headers: {'Authorization': 'token 56705191f857b83422eaea2516912bb1af2166da'}
   });
+  const User = require('../models/user')
+  const jwt = require('jsonwebtoken');
 
 class Anime{
     static showTopTen(req, res, next){
@@ -25,6 +27,33 @@ class Anime{
             .catch((err)=>{
                 console.log(err);
                 next(err)                
+            })
+    }
+
+    static searchAnime(req, res, next){
+        jikanApi
+            .get(`/search/anime?q=${req.params.name}`)
+            .then( ({ data }) => {
+                res.status(200).json(data)
+            })
+            .catch(err =>{
+                next(err)
+            })
+    }
+
+    static favAnime(req, res, next){
+        const {id} = req.body
+        console.log(req.headers.token);
+        const token = jwt.verify(req.headers.token, process.env.SECRET)
+        console.log(token,"ini token");
+        
+        User.findOneAndUpdate({_id: token.id}, {$push: {favorite:id}})
+            .then((data)=>{
+                console.log(data);
+                res.status(200).json(data)
+            })
+            .catch(err=>{
+                next(err)
             })
     }
 
